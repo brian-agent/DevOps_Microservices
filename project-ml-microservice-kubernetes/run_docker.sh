1757fae571d7b505830e79192cd9b5a42bca1427
tyan <65673958+brian-agent@users.noreply.github.com>
2023-02-20 11:18:50 +0300

    XLXLX

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -3,10 +3,11 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build --tag=kim1116/build .
 
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
-# Run flask app
+docker run -p 80:80 kim1116/build
+
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,11 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="docker.io/kim1116/build:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+docker push kim1116/build
